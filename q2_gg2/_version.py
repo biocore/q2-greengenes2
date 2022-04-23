5b414008a41c36539f712a73acd1d0b75c20bb8c
Daniel McDonald <danielmcdonald@ucsd.edu>
2022-04-23 16:57:37 -0700

    lint

--- q2_gg2/_version.py
+++ q2_gg2/_version.py
@@ -353,7 +353,8 @@ def git_pieces_from_vcs(tag_prefix, root, verbose, runner=run_command):
         pieces["distance"] = int(count_out)  # total number of commits
 
     # commit date: see ISO-8601 comment in git_versions_from_keywords()
-    date = runner(GITS, ["show", "-s", "--format=%ci", "HEAD"], cwd=root)[0].strip()
+    date = runner(GITS, ["show", "-s", "--format=%ci", "HEAD"],
+                  cwd=root)[0].strip()
     # Use only the last line.  Previous lines may contain GPG signature
     # information.
     date = date.splitlines()[-1]
@@ -443,10 +444,10 @@ def render_pep440_pre(pieces):
     if pieces["closest-tag"]:
         if pieces["distance"]:
             # update the post release segment
-            tag_version, post_version = pep440_split_post(pieces["closest-tag"])
+            tag_version, post_version = pep440_split_post(pieces["closest-tag"])  # noqa
             rendered = tag_version
             if post_version is not None:
-                rendered += ".post%d.dev%d" % (post_version+1, pieces["distance"])
+                rendered += ".post%d.dev%d" % (post_version+1, pieces["distance"])  # noqa
             else:
                 rendered += ".post0.dev%d" % (pieces["distance"])
         else:
